1bd23b8769ed75b948084dd81d5932b4d2a644cc
j1nma <j1nma@users.noreply.github.com>
2018-10-18 03:27:50 -0300

    Wordcount Mapper, Combiner, Reducer. Cluster (Client) joins Server.

--- client/src/main/java/ar/edu/itba/client/Cluster.java
+++ client/src/main/java/ar/edu/itba/client/Cluster.java
@@ -1,19 +1,134 @@
 package ar.edu.itba.client;
 
+import ar.edu.itba.combiner.WordcountCombinerFactory;
+import ar.edu.itba.mapper.TokenizerMapper;
+import ar.edu.itba.reducer.WordcountReducerFactory;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ICompletableFuture;
+import com.hazelcast.core.IMap;
+import com.hazelcast.mapreduce.Job;
+import com.hazelcast.mapreduce.JobTracker;
+import com.hazelcast.mapreduce.KeyValueSource;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 import java.util.Map;
 
+
 public class Cluster {
+	
+	private static final String[] DATA_RESOURCES_TO_LOAD = {"dracula.txt", "mobydick.txt", "2city10.txt"};
+	
+	private static final String MAP_NAME = "books";
+	
 	public static void main(String[] args) {
-		HazelcastInstance hz = Hazelcast.newHazelcastInstance();
-		Map<String, String> datos = hz.getMap("materias");
-		datos.put("72.42", "POD");
-		System.out.println(String.format("%d Datos en el cluster", datos.size()));
-		for (String key : datos.keySet()) {
-			System.out.println(String.format("Datos con key %s= %s", key,
-					datos.get(key)));
+		
+		HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+		
+		try {
+			// read data
+			fillMapWithData(hazelcastInstance);
+			
+			JobTracker tracker = hazelcastInstance.getJobTracker("word-to-books");
+			
+			IMap<String, String> map = hazelcastInstance.getMap(MAP_NAME);
+			
+			KeyValueSource<String, String> source = KeyValueSource.fromMap(map);
+			
+			Job<String, String> job = tracker.newJob(source);
+			
+			ICompletableFuture<Map<String, Integer>> future = job
+					.mapper(new TokenizerMapper())
+					// activate Combiner to add combining phase!
+					.combiner(new WordcountCombinerFactory())
+					.reducer(new WordcountReducerFactory())
+					.submit();
+			
+			System.out.println(ToStringPrettyfier.toString(future.get()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			// shutdown cluster
+			Hazelcast.shutdownAll();
+		}
+	}
+	
+	private static void fillMapWithData(HazelcastInstance hazelcastInstance) throws Exception {
+		
+		IMap<String, String> map = hazelcastInstance.getMap(MAP_NAME);
+		
+		for (String file : DATA_RESOURCES_TO_LOAD) {
+			InputStream is = Cluster.class.getClassLoader().getResourceAsStream(file);
+			LineNumberReader reader = new LineNumberReader(new InputStreamReader(is));
+			
+			String line;
+			StringBuilder sb = new StringBuilder();
+			while ((line = reader.readLine()) != null) {
+				sb.append(line).append("\n");
+			}
+			map.put(file, sb.toString());
+			
+			is.close();
+			reader.close();
+		}
+	}
+	
+	private static final class ToStringPrettyfier {
+		
+		private ToStringPrettyfier() {
+		}
+		
+		@SuppressWarnings("checkstyle:cyclomaticcomplexity")
+		private static String prettify(String toStringValue) {
+			int depth = 0;
+			
+			char[] chars = toStringValue.toCharArray();
+			
+			boolean openQuote = false;
+			boolean openDoubleQuote = false;
+			
+			StringBuilder sb = new StringBuilder(chars.length);
+			for (char c : chars) {
+				if (c == ',' && !openQuote && !openDoubleQuote) {
+					sb.append(',').append('\n');
+					indent(sb, depth);
+				} else if (c == '{' || c == '[') {
+					depth++;
+					sb.append(c).append('\n');
+					indent(sb, depth);
+				} else if (c == '}' || c == ']') {
+					depth--;
+					sb.append('\n');
+					indent(sb, depth);
+					sb.append(c);
+				} else if (c == '\'') {
+					if (!openDoubleQuote) {
+						openQuote = !openQuote;
+					}
+					sb.append(c);
+				} else if (c == '"') {
+					if (!openQuote) {
+						openDoubleQuote = !openDoubleQuote;
+					}
+					sb.append(c);
+				} else {
+					sb.append(c);
+				}
+			}
+			return sb.toString();
+		}
+		
+		private static void indent(StringBuilder sb, int depth) {
+			for (int o = 0; o < depth; o++) {
+				sb.append("  ");
+			}
+		}
+		
+		static String toString(Object value) {
+			return prettify(value.toString());
 		}
 	}
 }
--- server/src/main/java/ar/edu/itba/server/Server.java
+++ server/src/main/java/ar/edu/itba/server/Server.java
@@ -1,12 +1,26 @@
 package ar.edu.itba.server;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Server {
-    private static Logger logger = LoggerFactory.getLogger(Server.class);
+import static java.util.Collections.singletonList;
 
-    public static void main(String[] args) {
-        logger.info("pod Server Starting ...");
-    }
+/**
+ * Member node entry point class.
+ */
+public class Server {
+	private final static Logger LOGGER = LoggerFactory.getLogger(Server.class);
+	
+	public static void main(String[] args) {
+		LOGGER.info("Starting member node...");
+		
+		// prepare Hazelcast cluster
+		Hazelcast.newHazelcastInstance();
+		Hazelcast.newHazelcastInstance();
+		Hazelcast.newHazelcastInstance();
+	}
 }
